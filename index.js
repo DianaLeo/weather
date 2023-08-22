0982e514b68e1a0856712a302e4263b8e70a2e4f
Diana Liu <mata7890@sina.com>
2023-08-22 15:38:22 +1000

    Add readme

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
-const apikey = "5f60cb2249aca011f4e4556697623a57";//weather api是收费的，老师给的密钥
-//5f60cb2249aca011f4e4556697623a57
+const apikey = "5f60cb2249aca011f4e4556697623a57";//I registered an account
+//5f60cb2249aca011f4e4556697623a57//This is given by teacher
 
 const weatherData = document.getElementById('weather-data');
 const cityInput = document.getElementById('city-input');
 const formEl = document.querySelector('form');
+const httpRequestMethodBtn_fetch = document.getElementById('fetch');
+const httpRequestMethodBtn_XMLHttp = document.getElementById('xmlHttpRequest');
+const httpRequestMethodBtn_axios = document.getElementById('axios');
+
+
+let handler = getWeatherData;
 
 formEl.addEventListener('submit', (event) => {
     event.preventDefault();
     const cityValue = cityInput.value;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apikey}&units=metric`;
 
-    //getWeatherData(url);
-    //getWeatherData2(url);
-    getWeatherData3(url);
-
+    chooseHttpRequestMethod(handler,url);
 })
+
+httpRequestMethodBtn_fetch.onclick = ()=>{
+    handler = getWeatherData;
+}
+httpRequestMethodBtn_XMLHttp.onclick = ()=>{
+    handler = getWeatherData2;
+}
+httpRequestMethodBtn_axios.onclick = ()=>{
+    handler = getWeatherData3;
+}
+
+function chooseHttpRequestMethod(fn = getWeatherData,url){
+    fn(url);
+}
 
 //async await fetch
 async function getWeatherData(url) {
@@ -27,24 +44,9 @@ async function getWeatherData(url) {
         //fetch返回的是raw data，要自己解析
         const data = await response.json();
         console.log(data);
-        const temperature = Math.round(data.main.temp);
-        const description = data.weather[0].description;
-        const icon = data.weather[0].icon;
-        const details = [
-            `Feels like:${Math.round(data.main.feels_like)}`,
-            `Humidity:${data.main.humidity}%`,
-            `Wind speed:${data.wind.speed}m/s`];
-        console.log(temperature, description, icon, details);
-        console.log(weatherData);
-        weatherData.querySelector('.icon').innerHTML = `<img src='http://openweathermap.org/img/wn/${icon}.png'/>`;
-        weatherData.querySelector('.temperature').textContent = `${temperature}℃`;
-        weatherData.querySelector('.description').textContent = description;
-        weatherData.querySelector('.details').innerHTML = details.map((detail) => `<div>${detail}</div>`).join('');
+        displayData(data);
     } catch (error) {
-        weatherData.querySelector('.icon').innerHTML = '';
-        weatherData.querySelector('.temperature').textContent = '';
-        weatherData.querySelector('.description').textContent = 'An error occurred, please try again later';
-        weatherData.querySelector('.details').innerHTML = '';
+        errorHandler();
     }
 }
 
@@ -58,27 +60,12 @@ function getWeatherData2(url) {
     }
     xhttp.onload = function () {
         if (xhttp.status != 200) {
-            weatherData.querySelector('.icon').innerHTML = '';
-            weatherData.querySelector('.temperature').textContent = '';
-            weatherData.querySelector('.description').textContent = xhttp.status + ": " + xhttp.statusText_ + ", State:" + xhttp.readyState;
-            weatherData.querySelector('.details').innerHTML = '';
+            errorHandler();
             throw new Error('Network response was not ok');
             return;
-        } 
+        }
         const data = JSON.parse(xhttp.responseText);
-        const temperature = Math.round(data.main.temp);
-        const description = data.weather[0].description;
-        const icon = data.weather[0].icon;
-        const details = [
-            `Feels like:${Math.round(data.main.feels_like)}`,
-            `Humidity:${data.main.humidity}%`,
-            `Wind speed:${data.wind.speed}m/s`];
-        console.log(temperature, description, icon, details);
-        console.log(weatherData);
-        weatherData.querySelector('.icon').innerHTML = `<img src='http://openweathermap.org/img/wn/${icon}.png'/>`;
-        weatherData.querySelector('.temperature').textContent = `${temperature}℃`;
-        weatherData.querySelector('.description').textContent = description;
-        weatherData.querySelector('.details').innerHTML = details.map((detail) => `<div>${detail}</div>`).join('');
+        displayData(data);
     }
     xhttp.open('GET', url, true);
     xhttp.send();
@@ -92,24 +79,31 @@ async function getWeatherData3(url) {
         const response = await axios.get(url);
         const data = response.data;
         console.log(data);
-        const temperature = Math.round(data.main.temp);
-        const description = data.weather[0].description;
-        const icon = data.weather[0].icon;
-        const details = [
-            `Feels like:${Math.round(data.main.feels_like)}`,
-            `Humidity:${data.main.humidity}%`,
-            `Wind speed:${data.wind.speed}m/s`];
-        console.log(temperature, description, icon, details);
-        console.log(weatherData);
-        weatherData.querySelector('.icon').innerHTML = `<img src='http://openweathermap.org/img/wn/${icon}.png'/>`;
-        weatherData.querySelector('.temperature').textContent = `${temperature}℃`;
-        weatherData.querySelector('.description').textContent = description;
-        weatherData.querySelector('.details').innerHTML = details.map((detail) => `<div>${detail}</div>`).join('');
+        displayData(data);
     } catch (error) {
-        weatherData.querySelector('.icon').innerHTML = '';
-        weatherData.querySelector('.temperature').textContent = '';
-        weatherData.querySelector('.description').textContent = 'An error occurred, please try again later';
-        weatherData.querySelector('.details').innerHTML = '';
+        errorHandler();
     }
+}
 
+
+function displayData(data) {
+    const temperature = Math.round(data.main.temp);
+    const description = data.weather[0].description;
+    const icon = data.weather[0].icon;
+    const details = [
+        `Feels like:${Math.round(data.main.feels_like)}`,
+        `Humidity:${data.main.humidity}%`,
+        `Wind speed:${data.wind.speed}m/s`];
+    console.log(temperature, description, icon, details);
+    console.log(weatherData);
+    weatherData.querySelector('.icon').innerHTML = `<img src='http://openweathermap.org/img/wn/${icon}.png'/>`;
+    weatherData.querySelector('.temperature').textContent = `${temperature}℃`;
+    weatherData.querySelector('.description').textContent = description;
+    weatherData.querySelector('.details').innerHTML = details.map((detail) => `<div>${detail}</div>`).join('');
+}
+function errorHandler(){
+    weatherData.querySelector('.icon').innerHTML = '';
+    weatherData.querySelector('.temperature').textContent = '';
+    weatherData.querySelector('.description').textContent = 'An error occurred, please try again later';
+    weatherData.querySelector('.details').innerHTML = '';
 }
